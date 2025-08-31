c529674e82cfccb3bb4666fd2d648a1fd47f274b
vishwanathHiremath <vishush1701@gmail.com>
2025-08-31 16:15:16 +0530

    add guitar

---
+++ src/main/java/org/objectville/Guitar.java
@@ -0,0 +1,72 @@
+package org.objectville;
+
+public class Guitar {
+    private String serialNumber,builder,model,type,backWood,topWood;
+    private Double price;
+
+    public Guitar(String serialNumber, String builder, String model, String type, String backWood, String topWood, Double price) {
+        this.serialNumber = serialNumber;
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+        this.price = price;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBuilder() {
+        return builder;
+    }
+
+    public void setBuilder(String builder) {
+        this.builder = builder;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBackWood() {
+        return backWood;
+    }
+
+    public void setBackWood(String backWood) {
+        this.backWood = backWood;
+    }
+
+    public String getTopWood() {
+        return topWood;
+    }
+
+    public void setTopWood(String topWood) {
+        this.topWood = topWood;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
